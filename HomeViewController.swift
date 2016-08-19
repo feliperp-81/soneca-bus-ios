5636b7fe6b47d6f8aaf05b1334553d06c09f1437
Felipe Rodrigues <felipe.rodrigues@liferay.com>
2016-08-19 12:18:14 -0300

    Add NavigationViewController to storyboard

---
+++ HomeViewController.swift
@@ -0,0 +1,22 @@
+//
+//  HomeTableViewController.swift
+//  Soneca-Bus
+//
+//  Created by Felipe Rodrigues de Paula on 8/19/16.
+//  Copyright © 2016 Liferay. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
