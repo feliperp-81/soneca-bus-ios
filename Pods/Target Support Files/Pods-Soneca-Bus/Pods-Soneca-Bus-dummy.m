9595391c97384ee94b56b630ed85df3efc819d78
Felipe Rodrigues <felipe.rodrigues@liferay.com>
2016-08-19 11:34:48 -0300

    Add xcworkspace

---
+++ Pods/Target Support Files/Pods-Soneca-Bus/Pods-Soneca-Bus-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Soneca_Bus : NSObject
+@end
+@implementation PodsDummy_Pods_Soneca_Bus
+@end
---
+++ Pods/Target Support Files/Pods-Soneca-Bus/Pods-Soneca-Bus-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Soneca_BusVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Soneca_BusVersionString[];
+
