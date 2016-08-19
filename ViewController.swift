f484d26974b6dd9ea8bb3e2095ec71f654d665bb
Allan Melo <allan.melo@liferay.com>
2016-08-19 11:26:56 -0300

    Add project

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Soneca-Bus
+//
+//  Created by Allan Melo on 8/19/16.
+//  Copyright © 2016 Liferay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
